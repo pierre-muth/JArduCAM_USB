3ddcdcdac017f54d47faa20937910c54bc7f829c
muth <muth.inc@free.fr>
2019-11-19 14:25:59 +0100

    first access to dll

--- src/arducam/ArduCamSDK.java
+++ src/arducam/ArduCamSDK.java
@@ -19,22 +19,48 @@ public interface ArduCamSDK extends Library {
 		public int	u32Width;					// Image Width
 		public int	u32Height;					// Image Height
 		public int	u8PixelBytes;
-		public int   u8PixelBits;
+		public int  u8PixelBits;
 		public int  u32I2cAddr;
 		public int  u32Size;
-		public int   usbType;
-		public int emI2cMode;
-		public int emImageFmtMode;			// image format mode 
+		public int  usbType;
+		public int  emI2cMode;
+		public int  emImageFmtMode;			// image format mode 
 		public int	u32TransLvl;
+		
+	}
+	
+	@FieldOrder({ "stImagePara", "u64Time", "pu8ImageData" })
+	public static class ArduCamOutData extends Structure {
+		public static class ByReference extends ArduCamOutData implements Structure.ByReference { }
+		
+		public ArduCamCfg stImagePara;				
+		public int u64Time;      
+		public Pointer pu8ImageData;      
 		
 	}
 
 	int ArduCam_autoopen(IntByReference useHandle, ArduCamCfg.ByReference useCfg);
+	
 	int ArduCam_scan( Object... args );
 	int ArduCam_open( Pointer useHandle, ArduCamCfg.ByReference useCfg, int usbIdx );
 	int ArduCam_close( int useHandle );
 	
+	int ArduCam_beginCaptureImage( int useHandle );
+	int ArduCam_captureImage( int useHandle );
+	int ArduCam_endCaptureImage( int useHandle );
+	
+	int ArduCam_availableImage( int useHandle );
+	int ArduCam_readImage( int useHandle, Pointer pstFrameData );	
+	
+	int ArduCam_del( int useHandle );							
+	int ArduCam_flush( int useHandle );
+
 	int ArduCam_writeReg_16_16( int useHandle, int i2cAddr, int regAddr, int pval );
 	int ArduCam_readReg_16_16( int useHandle, int i2cAddr, int regAddr, Pointer pval );
+	
+	int ArduCam_setboardConfig( int useHandle, int u8Command, int u16Value, int u16Index, int u32BufSize, Pointer pu8Buf );
 
+	int ArduCam_isFrameReady(int useHandle);
+	int ArduCam_getSingleFrame(int useHandle, Pointer pstFrameData, int time_out);
+	int ArduCam_setMode(int useHandle, int mode);
 }
--- src/arducam/SDKtest.java
+++ src/arducam/SDKtest.java
@@ -19,7 +19,7 @@ public class SDKtest {
 		IntByReference useHandle = new IntByReference();
 		int usbIdx = 0;
 		final ArduCamSDK.ArduCamCfg.ByReference useCfg = new ArduCamSDK.ArduCamCfg.ByReference();
-		useCfg.u32CameraType = 0;
+		useCfg.u32CameraType = 0x4D091031;
 		useCfg.u16Vid = 0x52CB;                 // Vendor ID for USB
 		useCfg.u32Width = 1280;					// Image Width
 		useCfg.u32Height = 964;					// Image Height
@@ -30,7 +30,7 @@ public class SDKtest {
 		useCfg.usbType = 2;
 		useCfg.emI2cMode = 3;
 		useCfg.emImageFmtMode = 0;			// image format mode 
-		useCfg.u32TransLvl = 0;
+		useCfg.u32TransLvl = 64;
 		
 		try {
 			Thread.sleep(2000);
@@ -52,15 +52,54 @@ public class SDKtest {
 		int i2cAddr = 0x20;
 		int regAddr = 0x3012;
 		
-		answer = arduCamSDKlib.ArduCam_writeReg_16_16(useHandle.getValue(), i2cAddr, regAddr, 0xDEAD);
+		answer = arduCamSDKlib.ArduCam_writeReg_16_16(useHandle.getValue(), i2cAddr, regAddr, 0x10);
 		System.out.println("ArduCam_writeReg_16_16 returned: "+Utils.intToHex(answer));
 		System.out.println("");
 		
-		IntByReference pval = new IntByReference();
+		//#define CONTINUOUS_MODE	0x02
+		answer = arduCamSDKlib.ArduCam_setMode(useHandle.getValue(), 0x02);
+		System.out.println("ArduCam_setMode returned: "+Utils.intToHex(answer));
+		System.out.println("");
 		
+		IntByReference pval = new IntByReference();
 		answer = arduCamSDKlib.ArduCam_readReg_16_16(useHandle.getValue(), i2cAddr, regAddr, pval.getPointer());
 		System.out.println("ArduCam_readReg_16_16 returned: "+Utils.intToHex(answer));
 		System.out.println("pval.getValue(): "+Utils.intToHex( pval.getValue() ));
+		System.out.println("");
+		
+		answer = arduCamSDKlib.ArduCam_beginCaptureImage(useHandle.getValue());
+		System.out.println("ArduCam_beginCaptureImage returned: "+Utils.intToHex(answer));
+		System.out.println("");
+		
+
+		int i = 30;
+		while (i > 0) {
+			answer = arduCamSDKlib.ArduCam_captureImage(useHandle.getValue());
+			System.out.println("ArduCam_captureImage returned: "+Utils.intToHex(answer));
+			System.out.println("");
+			
+			answer = arduCamSDKlib.ArduCam_availableImage(useHandle.getValue());
+			System.out.println(i +" ArduCam_availableImage returned: "+Utils.intToHex(answer));
+			System.out.println("");
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			
+			i--;
+		}
+		
+		
+		final ArduCamSDK.ArduCamOutData.ByReference frameData = new ArduCamSDK.ArduCamOutData.ByReference();
+		answer = arduCamSDKlib.ArduCam_readImage(useHandle.getValue(), frameData.getPointer());
+		System.out.println("ArduCam_readImage returned: "+Utils.intToHex(answer));
+		System.out.println("");
+		
+		answer = arduCamSDKlib.ArduCam_endCaptureImage(useHandle.getValue());
+		System.out.println("ArduCam_endCaptureImage returned: "+Utils.intToHex(answer));
 		System.out.println("");
 		
 		answer = arduCamSDKlib.ArduCam_close(useHandle.getValue());
